c7391b4b0a0c3a1576b955a32060d3618cccf7d8
Christopher Melby <melby.christopher@gmail.com>
2020-02-22 06:32:28 -0800

    taking away slide copy

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -44,8 +44,6 @@ const Landing = () => {
       alt="First slide"
     />
     <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -57,8 +55,6 @@ const Landing = () => {
     />
 
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
